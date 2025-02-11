5c6716a6c25bbdd5c7eccc51cbc34a3d91b9600a
rjrizani <rjrizani@gmail.com>
2025-02-11 15:34:04 +0800

    add seo future

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, request
 
 app = Flask(__name__)
 
@@ -27,6 +27,18 @@ destinations = [
 @app.route('/')
 def index():
     return render_template('index.html', destinations=destinations)
+
+@app.route('/destination/<name>')
+def destination_detail(name):
+    destination = next((d for d in destinations if d['name'] == name), None)
+    if destination is None:
+        return 'Destination not found', 404
+    return render_template('destination_detail.html', destination=destination)
+
+@app.route('/robots.txt')
+@app.route('/sitemap.xml')
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:])
 
 if __name__ == '__main__':
     app.run(debug=True)
